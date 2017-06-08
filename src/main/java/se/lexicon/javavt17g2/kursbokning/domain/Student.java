0cac9cab477e8e4cb3460c29bf97af08c024bec1
Rickard Närström <rickard@narstrom.se>
2017-06-08 09:06:56 +0200

    Add many-to-many relation between student and course

--- src/main/java/se/lexicon/javavt17g2/kursbokning/domain/Student.java
+++ src/main/java/se/lexicon/javavt17g2/kursbokning/domain/Student.java
@@ -1,7 +1,13 @@
 package se.lexicon.javavt17g2.kursbokning.domain;
 
+import java.util.Collections;
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Student extends AbstractEntity {
@@ -9,4 +15,9 @@ public class Student extends AbstractEntity {
 	private String name;
 	public String getName() { return this.name; }
 	public void setName(String name) { this.name = name; }
+
+	@ManyToMany
+	private List<Course> courses;
+	@JsonIgnore
+	public List<Course> getCourses() { return Collections.unmodifiableList(this.courses); }
 }
